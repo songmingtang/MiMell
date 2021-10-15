8ec2f2f29194d047fa902e951ccd02343f1ad4b2
songmingtang <1550296923@qq.com>
2021-10-15 16:21:17 +0800

    vue
    Signed-off-by: songmingtang <1550296923@qq.com>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import App from './App.vue'
 
+//根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api';
+//设置超时时间(8s)
+axios.defaults.timeout = 8000
+    //接口错误拦截
+axios.interceptors.request.use(function(response) {
+    let res = response.data;
+    if (res.status == 0) {
+        return res.data;
+    } else if (res.status == 10) {
+        window.location.href = '/#/login'
+    } else {
+        alert(res.msg);
+    }
+})
+
+Vue.prototype.axios = axios
 Vue.config.productionTip = false
 
 new Vue({
